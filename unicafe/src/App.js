90fb28f2b6604f9cb494b854aae5a8f08d8ff319
spar931 <spar931@aucklanduni.ac.nz>
2023-01-06 16:27:25 +1300

    exercise 1.9 - only display statistics only once feedback has been gathered

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -2,11 +2,21 @@
 import { useState } from 'react'
 import Button from './Button'
 
-const Statistics = ({text, statistic}) => {
+const Statistics = ({stat1, stat2, stat3}) => {
+    if ((stat1 + stat2 + stat3) > 0) {
+        return (
+            <div>
+                <p>good {stat1}</p>
+                <p>neutral {stat2}</p>
+                <p>bad {stat3}</p>
+                <p>all {stat1 + stat2 + stat3}</p>
+                <p>average {((stat1 * 1) + (stat2 * 0) + (stat3 * -1)) / (stat1 + stat2 + stat3)}</p>
+                <p>positive {(stat1 / (stat2 + stat3 + stat1)) * 100}</p>
+            </div>
+        )
+    }
     return (
-        <div>
-            <p>{text} {statistic}</p>
-        </div>
+        <div><p>No Feedback Given</p></div>
     )
 }
 
@@ -23,12 +33,7 @@ const App = () => {
         <Button text='neutral' onClick={() => setNeutral(neutral + 1)} />
         <Button text='bad' onClick={() => setBad(bad + 1)} />
         <h2>Statistics</h2>
-        <Statistics text='good' statistic={good}/>
-        <Statistics text='neutral' statistic={neutral}/>
-        <Statistics text='bad' statistic={bad}/>
-        <Statistics text='all' statistic={good + neutral + bad}/>
-        <Statistics text='average' statistic={((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)}/>
-        <Statistics text='positive' statistic={(good / (neutral + bad + good)) * 100}/>
+        <Statistics stat1={good} stat2={neutral} stat3={bad}/>
     </div>
   )
 }
