76de94fa64bce4eb5abf05f42f51f7bc6f808208
spar931 <spar931@aucklanduni.ac.nz>
2023-01-06 15:44:19 +1300

    exercise 1.7 - add all, average and positive % in statistics

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Button from './Button'
 
 const App = () => {
+  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -17,6 +18,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)}</p>
+        <p>positive {(good / (neutral + bad + good)) * 100}</p>
     </div>
   )
 }
