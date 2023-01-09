d3c9d085bb60e9e010d906a92e119bcd7f02c1a2
spar931 <spar931@aucklanduni.ac.nz>
2023-01-09 13:19:56 +1300

    exercise 2.1 - replace array index hardcoding with map function to display course parts

--- courseinformation/src/App.js
+++ courseinformation/src/App.js
@@ -1,36 +1,42 @@
 // Exercises 1.1 to 1.5
-
+// Exercises 2.1 to 2.5
 import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
-  
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
     </div>
   )
-
 }
 
 export default App
--- courseinformation/src/Content.js
+++ courseinformation/src/Content.js
@@ -1,10 +1,12 @@
 const Content = ({parts}) => {
-    console.log(parts)
     return (
       <div>
-        <Part name={parts[0].name} exercises={parts[0].exercises} />
-        <Part name={parts[1].name} exercises={parts[1].exercises} />
-        <Part name={parts[2].name} exercises={parts[2].exercises} />
+        {parts.map(part => 
+          <Part key={part.id}
+            name={part.name}
+            exercises={part.exercises}
+          />
+        )}
       </div>
     )
   }
