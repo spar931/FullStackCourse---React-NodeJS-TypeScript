a6c925154602d4e5e2651f5029b52e3f7c3dfee8
spar931 <spar931@aucklanduni.ac.nz>
2023-01-10 16:02:31 +1300

    exercise 2.9 - allow users to search for a contact name

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,16 +3,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'arto hellas' , number: '040-1234567'}
+    { name: 'arto hellas', number: '040-123456', id: 1 },
+    { name: 'ada lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'dan abramov', number: '12-43-234345', id: 3 },
+    { name: 'mary poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const filteredPersons = persons.filter(person => person.name.includes(newFilter))
 
   const handleNameChange = (event) => {
     setNewName(event.target.value.toLowerCase())
   }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value.toLowerCase())
   }
 
   const addNameAndNumber = (event) => {
@@ -32,14 +41,16 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>filter shown with <input onChange={handleFilterChange}/></div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addNameAndNumber}>
-        <div>name: <input value={newName} onChange={handleNameChange} /></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => 
+          {filteredPersons.map(person => 
           <li key={person.name}>
             {person.name} {person.number}
           </li>
