b0563a713ec3b97b4da7ddc6ad65429f65aa66d8
spar931 <spar931@aucklanduni.ac.nz>
2023-01-10 17:07:14 +1300

    exercise 2.10 - refactor by extracting suitable parts into separate components

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -12,7 +12,6 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
-  const filteredPersons = persons.filter(person => person.name.includes(newFilter))
 
   const handleNameChange = (event) => {
     setNewName(event.target.value.toLowerCase())
@@ -42,19 +41,53 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>filter shown with <input onChange={handleFilterChange}/></div>
-      <h2>Add a new contact</h2>
-      <form onSubmit={addNameAndNumber}>
-        <div>name: <input value={newName} onChange={handleNameChange}/></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div><button type="submit">add</button></div>
-      </form>
+      <Filter handleFilterChange={handleFilterChange} />
+      <PersonForm 
+        addNameAndNumber={addNameAndNumber} 
+        handleNameChange={handleNameChange} 
+        handleNumberChange={handleNumberChange}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <Persons persons={persons} search={newFilter} />
+    </div>
+  )
+}
+
+
+const Filter = ({handleFilterChange}) => {
+  return (
+    <div>
+      filter shown with <input onChange={handleFilterChange}/>
+    </div>
+  )
+}
+
+
+const Persons = ({persons, search}) => {
+  const filteredPersons = persons.filter(person => person.name.includes(search))
+  return (
+    <div>
       <h2>Numbers</h2>
-          {filteredPersons.map(person => 
+        {filteredPersons.map(person => 
           <li key={person.name}>
             {person.name} {person.number}
           </li>
         )}
+    </div>
+  )
+}
+
+
+const PersonForm = ({addNameAndNumber, handleNameChange, handleNumberChange, newName, newNumber}) => {
+  return (
+    <div>
+      <h2>Add a new contact</h2>  
+        <form onSubmit={addNameAndNumber}>
+          <div>name: <input value={newName} onChange={handleNameChange}/></div>
+          <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+          <div><button type="submit">add</button></div>
+        </form>
     </div>
   )
 }
