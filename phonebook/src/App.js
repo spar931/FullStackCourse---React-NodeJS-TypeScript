f774466ab8ffcfafbf74f6c952d2c8bde6360951
spar931 <spar931@aucklanduni.ac.nz>
2023-01-11 14:23:26 +1300

    exercise 2.11 - fetch json data with an Effect hook from the server using the axios library

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,13 +1,20 @@
-// Exercises 2.6 to 2.10
-import { useState } from 'react'
+// Exercises 2.6 to 2.11
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'arto hellas', number: '040-123456', id: 1 },
-    { name: 'ada lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'dan abramov', number: '12-43-234345', id: 3 },
-    { name: 'mary poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {    
+    console.log('effect')    
+    axios      
+      .get('http://localhost:3001/persons')      
+      .then(response => {        
+        console.log('promise fulfilled')        
+        setPersons(response.data)      
+      })  
+    }, [])  
+    console.log('render', persons.length, 'persons')
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
