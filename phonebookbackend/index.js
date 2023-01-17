0058ddc55b149bfa859861ca18e0c6c8c81d0726
spar931 <spar931@aucklanduni.ac.nz>
2023-01-18 12:20:05 +1300

    exercise 3.6 - implement error handling for when name already exists in the phonebook or when name or number is missing

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -59,9 +59,15 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body)
   
-  if (!body.name || !body.number){
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'content missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
     })
   }
 
