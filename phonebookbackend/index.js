52db7ae99eee91e7913cc32ecb010e941f16d669
spar931 <spar931@aucklanduni.ac.nz>
2023-01-17 16:32:27 +1300

    exercise 3.3 & 3.4 - implement a GET person and DELETE person request and test with VSC REST Client

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -30,7 +30,25 @@ app.get('/', (request, response) => {
 
 app.get('/info', (request, response) => {  
   response.send(`Phonebook has info for ${persons.length} people - 
-  ${request.header.date}`)
+  ${Date()}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {  
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {    
+    response.json(person)  
+  } else {    
+    response.status(404).end()  
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
