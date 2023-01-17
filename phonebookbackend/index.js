cda3e89a2c2ef153bf5e9ebd7e896d7b46c1e7fc
spar931 <spar931@aucklanduni.ac.nz>
2023-01-18 12:12:58 +1300

    exercise 3.5 - implement POST request to add entries to phonebook

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -24,10 +25,6 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/info', (request, response) => {  
   response.send(`Phonebook has info for ${persons.length} people - 
   ${Date()}`)
@@ -49,6 +46,34 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+  
+  if (!body.name || !body.number){
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
