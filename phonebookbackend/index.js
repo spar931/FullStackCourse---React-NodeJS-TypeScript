71c40343dd65d1ca3f81d3497fae8a30f6c8a505
spar931 <spar931@aucklanduni.ac.nz>
2023-02-04 14:14:10 +1300

    exercise 3.17 & 3.18 - allow updating of numbers and allow for GET of a person from db by id

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -17,11 +17,6 @@ morgan.token('content', function (req, res) {
   return JSON.stringify(req.body)
 })
 
-app.get('/info', (request, response) => {  
-  response.send(`Phonebook has info for ${persons.length} people - 
-  ${Date()}`)
-})
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -44,6 +39,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
