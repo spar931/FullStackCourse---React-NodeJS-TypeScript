7a7022b5d9f4b8e6dcbfdccaed223bbc866626bf
spar931 <spar931@aucklanduni.ac.nz>
2023-02-11 16:56:34 +1300

    exercise 4.4 - define a function to count all likes from a list of blogs and verify it passes the test

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  let sumLikes = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < blogs.length; i++) {
+    sumLikes += blogs[i].likes;
+  }
+  return sumLikes;
+};
+
 module.exports = {
-  dummy,
+  totalLikes, dummy,
 };
